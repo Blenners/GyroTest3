e5de8d3fbe3a65c3c8b4353321df980bcfaa1e19
Blenners <36538956+Blenners@users.noreply.github.com>
2018-04-29 11:54:15 +0100

    Final Version :)
    Added:
    
     - Count down then reset after connecting, do as a seperate thread UNTESTED
    
    Potential TODOs:
    
     - Add vibration when looking too far, link it into Restrivted vals function
    
     - Allow IP and Port to be changed from the UI

--- app/src/main/java/com/example/gyrotest/MainActivity.java
+++ app/src/main/java/com/example/gyrotest/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.app.Activity;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -79,12 +80,30 @@ public class MainActivity extends Activity {
             }
         }
     }*/
+    public  EditText waitBox = (EditText)findViewById(R.id.waitTime);
+    String editTextStr = waitBox.getText().toString();
+    public  Thread conthread = new Thread(new Runnable() { // Creates this as a thread
+
+        @Override
+        public void run() {
+            int waitTime = Integer.parseInt(editTextStr);
+            try {
+                conthread.sleep((waitTime)*1000);
+                worker.ResetPos();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    });
 
     //final Button b = findViewById(R.id.ConnectButton);
     public void Connect(View v){ // Called when the connect button is called
         try {                     // (Option in button setting)
             //b.setBackgroundColor(Color.GREEN);
-            worker.ResetPos();
+            //worker.ResetPos();
+            conthread.start();
             SH.connect();         // Calls connect function in SH (SocketHandler) class
         } catch (IOException e) {
             e.printStackTrace();  // Error handling
